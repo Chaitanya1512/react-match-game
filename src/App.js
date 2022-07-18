7efa3919cae590353e7b8af1828dc98173f91a26
Chaitanya <thakur.chaitanya25@gmail.com>
2022-07-18 21:53:59 +0530

    shuffling array of cards

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import "./App.css";
+
+const cardImages = [
+  { src: "/img/bulbasaur-1.png" },
+  { src: "/img/charmander-1.png" },
+  { src: "/img/eevee-1.png" },
+  { src: "/img/pikachu-1.png" },
+  { src: "/img/sandshrew-1.png" },
+  { src: "/img/snorlax-1.png" },
+];
+
+function App() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+
+    setCards(shuffledCards);
+    setTurns(0);
+  };
+
+  console.log(cards, turns);
+
+  return (
+    <div className="App">
+      <h1>Match Game</h1>
+      <button onClick={shuffleCards}>New Game</button>
+    </div>
+  );
+}
+
+export default App;
